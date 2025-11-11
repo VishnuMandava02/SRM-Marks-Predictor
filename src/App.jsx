fcb2209ddacf13fba006ce799ad901aedd5d14c6
Ramkumarakula <ramkumarakula66@gmail.com>
2025-11-11 17:47:00 +0530

    feat: Initial commit of React Grade Predictor MVP

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GradePredictor from './GradePredictor'; // Import the new component
+
+function App() {
+  return (
+    <div className="App">
+      {/* The component will handle its own styling */}
+      <GradePredictor />
+    </div>
+  );
+}
+
+export default App;
